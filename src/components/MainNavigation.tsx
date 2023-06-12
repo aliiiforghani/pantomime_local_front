9a2b5ad4744668bf195f0dc5986b626fd8ad0046
ali forghani <forgani.ali@Gmail.com>
2023-06-12 19:57:02 +0330

    Update MainNavigation.tsx

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -7,7 +7,7 @@ const MainNavigation = () => {
 
   return (
     <header
-      className={`shadow-md mb-10 top-0 transition-all duration-200 bg-white ${
+      className={`shadow-md mb-10 top-0 transition-all duration-200 bg-white dark:bg-gray-900 ${
         isLoading ? "blur-sm opacity-70" : "opacity-100 blur-0"
       }`}
     >
@@ -15,25 +15,31 @@ const MainNavigation = () => {
         <ul className=" items-center  flex justify-between py-2 container xl:max-w-screen-xl">
           <li></li>
           <li>
-            <Link className="block py-2  hover:text-amber-400" href="/">
+            <Link
+              className="block py-2 text-gray-900 dark:text-white  hover:text-amber-400"
+              href="/"
+            >
               خانه
             </Link>
           </li>
 
           <li>
-            <Link className="block py-2  hover:text-amber-400" href="/">
+            <Link
+              className="block py-2  text-gray-900 dark:text-white  hover:text-amber-400"
+              href="/"
+            >
               بلاگ ها
             </Link>
           </li>
 
           {user ? (
-            <span className="hover:text-amber-400">
+            <span className="hover:text-amber-400  text-gray-900 dark:text-white ">
               خوش آمدید {user.first_name}
             </span>
           ) : (
             <li>
               <Link
-                className="block py-2 hover:underline hover:text-amber-400"
+                className="block py-2 hover:underline  text-gray-900 dark:text-white  hover:text-amber-400"
                 href="/auth"
               >
                 ورود
